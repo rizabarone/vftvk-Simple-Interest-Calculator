7981cb6911cf759b029ccd67855d1a65501a7dc4
Riza Barone <ysa_e@hotmail.com>
2022-10-26 18:06:53 -0400

    changes to js file

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ function compute() {
     var principal = parseFloat(document.getElementById("principal").value);
     var rate = parseFloat(document.getElementById("rate").value);
     var years = parseFloat(document.getElementById("years").value);
-    var result = principal * years * rate /100;
-    var years_modified = new Date().getFullYear() + parseInt(years);
-    
-    document.getElementById("result").innerHTML = "If you deposit<mark>"
-    + principal + "</mark> , <br/> at an interest rate of<mark>"
-    + rate + "%</mark><br/>. You will receive an amount of<mark>" + result +
-    ",</mark><br/>in the year<mark>" + years_modified + "</mark><br/>";
-}
+    var result = principal * years * rate / 100;
+    var years_result = new Date().getFullYear() + parseInt(years);
+  
+    document.getElementById("result").innerHTML = `
+      If you deposit <mark>${principal}</mark>
+      <br/> at an interest rate of
+      <mark>${rate}%</mark><br/>.
+      You will receive an amount of <mark>${result}</mark>
+      <br/> in the year <mark>${years_result}</mark><br/>`;
+  }
         
 
 function updateRate(onchange) 
