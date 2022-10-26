38fc28a71fdf48b2cb977f26b04b7ce421121696
Riza Barone <ysa_e@hotmail.com>
2022-10-26 18:19:50 -0400

    changes

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ function compute() {
     var years_result = new Date().getFullYear() + parseInt(years);
   
     document.getElementById("result").innerHTML = `
-      If you deposit <mark>${principal}</mark>
+      If you deposit <mark> ${principal} </mark>
       <br/> at an interest rate of
-      <mark>${rate}%</mark><br/>.
-      You will receive an amount of <mark>${result}</mark>
-      <br/> in the year <mark>${years_result}</mark><br/>`;
+      <mark> ${rate} %</mark><br/>.
+      You will receive an amount of <mark> ${result} </mark>
+      <br/> in the year <mark> ${years_result} </mark><br/>`;
+
+      if (principal == 0) || (principal < 0) {
+          alert("Enter a positive number");
+      }
   }
         
 
@@ -20,3 +24,5 @@ function updateRate(onchange)
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
+
