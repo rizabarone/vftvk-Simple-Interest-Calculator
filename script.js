73f9022879a0c3e8198c5b338a53121450c9db05
Riza Barone <ysa_e@hotmail.com>
2022-10-26 17:40:00 -0400

    changes

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementByIdementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var result = principal * years * rate /100;
+    var years = new Date().getFullYear()+parseInt(years);
 }
         
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
