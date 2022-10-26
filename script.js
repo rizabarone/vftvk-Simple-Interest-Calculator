bdbb218429c2a038d0729a610102b15f03edb3e3
Riza Barone <ysa_e@hotmail.com>
2022-10-26 17:51:53 -0400

    another change

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function compute() {
     var years = parseFloat(document.getElementById("years").value);
     var result = principal * years * rate /100;
     var years = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
 }
         
 
-function updateRate() 
+function updateRate(onchange) 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
